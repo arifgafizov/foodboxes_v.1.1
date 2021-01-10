98fd2f24ffe17df47e619d0df86d103f5107a4b0
Arif_Gafizov <agafizov@gmail.com>
2021-01-10 19:43:45 +0300

    Update settings.py and add initial.py of item model

--- foodboxes/foodboxes/settings.py
+++ foodboxes/foodboxes/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'app_items',
+    'app_reviews',
+    'app_users'
 ]
 
 MIDDLEWARE = [
@@ -118,3 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ITEMS_IMAGE_DIR = 'items_images'
+
+AUTH_USER_MODEL = 'app_users.User'
