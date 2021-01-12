262ea622c577cd205e4037423c0543b1a7fe357f
Arif_Gafizov <agafizov@gmail.com>
2021-01-12 20:41:24 +0300

    Update DB and change model User, Review

--- foodboxes/app_reviews/models.py
+++ foodboxes/app_reviews/models.py
@@ -6,7 +6,7 @@ from app_users.models import User
 
 class Review(models.Model):
     author = models.ForeignKey(User, related_name='reviews', on_delete=models.CASCADE)
-    content = models.TextField()
+    text = models.TextField()
     created_at = models.DateTimeField()
     published_at = models.DateTimeField()
     status = models.CharField(max_length=100, choices=statusChoices)
--- foodboxes/app_users/models.py
+++ foodboxes/app_users/models.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class User(AbstractUser):
-    surname = models.CharField(max_length=100)
-    name = models.CharField(max_length=100)
-    patronymic = models.CharField(max_length=100)
+    middle_name = models.CharField(max_length=100)
     phone_number = models.CharField(max_length=100)
+    address = models.CharField(max_length=100)
+
