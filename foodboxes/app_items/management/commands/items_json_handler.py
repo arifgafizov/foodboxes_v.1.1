7ccda9bec6eda95602cc21db515975403fe0d146
Arif_Gafizov <agafizov@gmail.com>
2021-01-13 21:23:08 +0300

    Update items_json_handler.py

--- foodboxes/app_items/management/commands/items_json_handler.py
+++ foodboxes/app_items/management/commands/items_json_handler.py
@@ -1,8 +1,6 @@
 import requests
 
 from django.core.management.base import BaseCommand
-from rest_framework import status
-from rest_framework.response import Response
 
 from app_items.models import Item
 
@@ -11,29 +9,28 @@ class Command(BaseCommand):
     help = 'create objects for model Item'
 
     def handle(self, *args, **kwargs):
-        foodboxes = requests.get('https://stepik.org/media/attachments/course/73594/foodboxes.json')
+        foodboxes = requests.get('https://raw.githubusercontent.com/stepik-a-w/drf-project-boxes/master/foodboxes.json')
 
         if foodboxes:
             foodboxes_json = foodboxes.json()
 
             for foodbox in foodboxes_json:
-                item = Item.objects.filter(id=foodbox['inner_id']).first()
+                item = Item.objects.filter(id=foodbox['id']).first()
                 if not item:
                     new_item = Item.objects.create(
                         title=foodbox['name'],
-                        description=foodbox['about'],
-                        image=foodbox['images']['full'],
+                        description=foodbox['description'],
+                        image=foodbox['image'],
                         weight=foodbox['weight_grams'],
                         price=foodbox['price']
                     )
-                    self.stdout.write(f"The item {foodbox['name']} is created")
+                    self.stdout.write(f"The item {new_item.title} is created")
                 else:
                     item.save()
-                    self.stdout.write(f"The item {foodbox['name']} updated")
-
+                    self.stdout.write(f"The item {item.title} updated")
 
         elif foodboxes.status_code == 408:
-            return Response(status=status.HTTP_408_REQUEST_TIMEOUT)
+            return self.stdout.write("408 REQUEST TIMEOUT")
 
         else:
-            return Response(status=status.HTTP_404_NOT_FOUND)
+            return self.stdout.write("404 NOT FOUND")
