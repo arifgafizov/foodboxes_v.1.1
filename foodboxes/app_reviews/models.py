00c434b64ae499d4a0ee190da9aa498c5b6b1e0c
Arif_Gafizov <agafizov@gmail.com>
2021-01-17 18:02:53 +0300

    Update Review model

--- foodboxes/app_reviews/models.py
+++ foodboxes/app_reviews/models.py
@@ -2,12 +2,11 @@ from django.db import models
 
 from app_reviews.data import statusChoices
 from app_users.models import User
-from django.utils import timezone
 
 
 class Review(models.Model):
     author = models.ForeignKey(User, related_name='reviews', on_delete=models.CASCADE)
     text = models.TextField()
     created_at = models.DateTimeField()
-    published_at = models.DateTimeField(default=timezone.now(), null=True)
+    published_at = models.DateTimeField(null=True)
     status = models.CharField(max_length=100, choices=statusChoices)
