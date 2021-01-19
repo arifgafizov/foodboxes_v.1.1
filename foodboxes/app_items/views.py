a6643d9799b210b56b678da5ed6d177b1ceee5e6
Arif_Gafizov <agafizov@gmail.com>
2021-01-19 22:30:11 +0300

    Add list function to ItemViewset

--- foodboxes/app_items/views.py
+++ foodboxes/app_items/views.py
@@ -6,6 +6,11 @@ from app_items.models import ItemSerializer, Item
 
 
 class ItemViewset(ViewSet):
+    def list(self, request):
+        queryset = Item.objects.all()
+        serializer = ItemSerializer(queryset, many=True)
+        return Response(serializer.data)
+
     def retrieve(self, request, pk=None):
         queryset = Item.objects.all()
         item = get_object_or_404(queryset, pk=pk)
