2de3a994e1980c98ca6ef3ac500f129a5d209d2a
Arif_Gafizov <agafizov@gmail.com>
2021-01-26 12:29:16 +0300

    Update items_json_handler.py

--- foodboxes/items/management/commands/items_json_handler.py
+++ foodboxes/items/management/commands/items_json_handler.py
@@ -2,13 +2,13 @@ import os
 import requests
 
 from django.core.management.base import BaseCommand
+from django.conf import settings
 
 from items.models import Item
-from foodboxes.settings import MEDIA_ITEMS_IMAGE_DIR
 
 
 class Command(BaseCommand):
-    help = 'create objects for model Item'
+    help = 'create/update objects for model Item'
 
     def handle(self, *args, **kwargs):
         def parser_images(responses):
@@ -20,23 +20,26 @@ class Command(BaseCommand):
                 print(name_img, 'successfully downloaded to ./media/items_images')
 
                 p = requests.get(url_img)
-                out = open('./media/' + MEDIA_ITEMS_IMAGE_DIR + '/' + name_img, "wb")
+                out = open('./media/' + settings.MEDIA_ITEMS_IMAGE_DIR + '/' + name_img, "wb")
                 out.write(p.content)
                 out.close()
 
-        foodboxes = requests.get('https://raw.githubusercontent.com/stepik-a-w/drf-project-boxes/master/foodboxes.json')
+        url = 'https://raw.githubusercontent.com/stepik-a-w/drf-project-boxes/master/foodboxes.json'
+        foodboxes = requests.get(url)
 
         os.makedirs('./media/items_images', exist_ok=True)
-        if foodboxes:
+
+        try:
             parser_images(foodboxes)
             foodboxes_json = foodboxes.json()
 
             for foodbox in foodboxes_json:
+                path_to_image = settings.MEDIA_ITEMS_IMAGE_DIR + '/' + foodbox['image'].split('/')[-1]
                 values_for_update = {}
 
                 values_for_update['title'] = foodbox['title']
                 values_for_update['description'] = foodbox['description']
-                values_for_update['image'] = MEDIA_ITEMS_IMAGE_DIR + '/' + foodbox['image'].split('/')[-1]
+                values_for_update['image'] = path_to_image
                 values_for_update['weight'] = foodbox['weight_grams']
                 values_for_update['price'] = foodbox['price']
 
@@ -44,8 +47,5 @@ class Command(BaseCommand):
                     id=foodbox['id'], defaults=values_for_update
                 )
 
-        elif foodboxes.status_code == 408:
-            return self.stdout.write("408 REQUEST TIMEOUT")
-
-        else:
-            return self.stdout.write("404 NOT FOUND")
+        except requests.exceptions.RequestException as er:
+            print("Some Ambiguous Exception:", er)
