3a97588af68ca952c3c04b6b8241fca6a514519e
Arif_Gafizov <agafizov@gmail.com>
2021-01-28 23:58:28 +0300

    Add ItemList, ItemDetail

--- foodboxes/items/views.py
+++ foodboxes/items/views.py
@@ -1,19 +1,20 @@
-from django.shortcuts import get_object_or_404
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import OrderingFilter
+from rest_framework.generics import ListAPIView, RetrieveAPIView
 
 from .models import Item
+from .serializers import ItemSerializer
+from .filters import ItemFilter
 
 
-@api_view(http_method_names=['GET'])
-def function_based(request, pk):
-    item = get_object_or_404(Item, pk=pk)
-    response = {
-        "id": item.id,
-        "title": item.title,
-        "description": item.description,
-        "image": item.image.url,
-        "weight": item.weight,
-        "price": item.price
-    }
-    return Response(response)
+class ItemList(ListAPIView):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    filterset_class = ItemFilter
+    filterset_fields = ['price']
+    ordering = ['price']
+
+class ItemDetail(RetrieveAPIView):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
