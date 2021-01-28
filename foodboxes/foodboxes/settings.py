3dff426fab59a9d489f11cd82024e87cc128b401
Arif_Gafizov <agafizov@gmail.com>
2021-01-28 23:48:47 +0300

    Update settings.py, urls.py

--- foodboxes/foodboxes/settings.py
+++ foodboxes/foodboxes/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'drf_yasg',
     'rest_framework',
+    'django_filters',
     'items',
     'reviews',
     'users',
@@ -73,6 +75,12 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'foodboxes.wsgi.application'
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 6,
+}
 
 
 # Database
