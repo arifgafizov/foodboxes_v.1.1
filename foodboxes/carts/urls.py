d20b5a1aeb838cf893bb75e4c456fdb4b6129b6d
Arif_Gafizov <agafizov@gmail.com>
2021-01-29 22:45:13 +0300

    Add path carts/items/ and carts/items/<int:pk>/

--- foodboxes/carts/urls.py
+++ foodboxes/carts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import CartList
-
+from .views import CartList, CartItemList, CartItemDetail
 
 urlpatterns = [
     path('carts/', CartList.as_view(), name='carts_list'),
+    path('carts/items/', CartItemList.as_view(), name='carts_items_list'),
+    path('carts/items/<int:pk>/', CartItemDetail.as_view(), name='carts_items_detail'),
 ]
