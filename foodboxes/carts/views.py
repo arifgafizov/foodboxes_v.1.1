a2d5cb1de466cf0ddb3eec8ab94cbfed4f18d167
Arif_Gafizov <agafizov@gmail.com>
2021-01-29 22:45:47 +0300

    Add CartItemList and CartItemDetail views

--- foodboxes/carts/views.py
+++ foodboxes/carts/views.py
@@ -1,9 +1,19 @@
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
-from .models import Cart
-from .serializers import CartSerializer
+from .models import Cart, CartItem
+from .paginations import CartItemLimitOffsetPagination
+from .serializers import CartSerializer, CartItemSerializer
 
 
 class CartList(ListAPIView):
     queryset = Cart.objects.all()
     serializer_class = CartSerializer
+
+class CartItemList(ListCreateAPIView):
+    queryset = CartItem.objects.all()
+    serializer_class = CartItemSerializer
+    pagination_class = CartItemLimitOffsetPagination
+
+class CartItemDetail(RetrieveUpdateDestroyAPIView):
+    queryset = CartItem.objects.all()
+    serializer_class = CartItemSerializer
