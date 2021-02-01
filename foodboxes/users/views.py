f18a7ac603ac20295ca66ce1a0231eac59806604
Arif_Gafizov <agafizov@gmail.com>
2021-02-01 23:30:43 +0300

    Create CurrentUserRetrieveUpdateView, RegisterUser, CustomAuthToken

--- foodboxes/users/views.py
+++ foodboxes/users/views.py
@@ -1,3 +1,43 @@
-# from django.shortcuts import render
+from rest_framework import status
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.generics import CreateAPIView, RetrieveUpdateAPIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
 
-# Create your views here.
+from .models import User
+from .serializers import RegisterSerializer, UserSerializer
+
+
+class CurrentUserRetrieveUpdateView(RetrieveUpdateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+    authentication_classes = (TokenAuthentication,)
+
+    def get_object(self):
+        return self.request.user
+
+class RegisterUser(CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        data = serializer.data
+
+        return Response(data, status=status.HTTP_201_CREATED)
+
+class CustomAuthToken(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data,
+                                           context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            'token': token.key,
+        })
