b34b2d343b4b9fcc3007785f1e8446b95dd7b436
Arif_Gafizov <agafizov@gmail.com>
2021-02-01 23:18:02 +0300

    Update total_cost and total price fields

--- foodboxes/carts/models.py
+++ foodboxes/carts/models.py
@@ -1,3 +1,5 @@
+from decimal import Decimal
+
 from django.db import models
 from django.utils.translation import ugettext as _
 
@@ -6,11 +8,13 @@ from users.models import User
 
 
 class Cart(models.Model):
-    items = models.ManyToManyField(to=Item, through="CartItem", related_name='carts')
+    items = models.ManyToManyField(to=Item, through="CartItem")
     user = models.ForeignKey(User, related_name='carts', on_delete=models.CASCADE)
 
+    @property
     def total_cost(self):
-        return 'stuff'
+        return self.cart_items.price * self.cart_items.quantity
+
 
 class CartItem(models.Model):
     item = models.ForeignKey(Item, related_name='cart_items', on_delete=models.CASCADE)
@@ -18,12 +22,14 @@ class CartItem(models.Model):
     quantity = models.PositiveIntegerField()
     price = models.DecimalField(max_digits=13, decimal_places=2)
 
-    def total_price(self):
-        return 'stuff'
-
     class Meta:
         verbose_name = _('cart')
         verbose_name_plural = _('carts')
 
     def __str__(self):
         return 'cart of ' + self.user.last_name
+
+    @property
+    def total_price(self):
+        result = self.price * Decimal(self.quantity)
+        return result
