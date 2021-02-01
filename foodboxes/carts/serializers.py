d163a4e2167cf198ca46281e202a28144657eab2
Arif_Gafizov <agafizov@gmail.com>
2021-02-01 23:18:19 +0300

    Update CartItemSerializer

--- foodboxes/carts/serializers.py
+++ foodboxes/carts/serializers.py
@@ -1,11 +1,10 @@
-from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 
 from .models import Cart, CartItem
+from items.serializers import ItemSerializer
 
 
 class CartSerializer(ModelSerializer):
-    total_cost = serializers.ReadOnlyField()
 
     class Meta:
         model = Cart
@@ -13,9 +12,10 @@ class CartSerializer(ModelSerializer):
         read_only_fields = ['id', 'items']
 
 class CartItemSerializer(ModelSerializer):
-    total_price = serializers.ReadOnlyField()
+    item = ItemSerializer(many=True)
+    car = CartSerializer(many=True)
 
     class Meta:
         model = CartItem
-        fields = ['id', 'item', 'quantity', 'price', 'total_price']
+        fields = ['id', 'item', 'quantity', 'price', 'car', 'total_price']
         read_only_fields = ['id', 'price']
