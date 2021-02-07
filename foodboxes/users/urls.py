2b46b8a18ad915a41d35b189bed6d8458f22ae26
Arif_Gafizov <agafizov@gmail.com>
2021-02-07 21:34:42 +0300

    Update users urls.py

--- foodboxes/users/urls.py
+++ foodboxes/users/urls.py
@@ -5,6 +5,5 @@ from users.views import RegisterUserView, CustomAuthToken, CurrentUserRetrieveUp
 urlpatterns = [
     path('auth/login/', CustomAuthToken.as_view(), name='login'),
     path('auth/register/', RegisterUserView.as_view(), name='register'),
-    path('auth/current/', CurrentUserRetrieveUpdateView.as_view(), name='current'),
-
+    path('current/', CurrentUserRetrieveUpdateView.as_view(), name='current'),
 ]
