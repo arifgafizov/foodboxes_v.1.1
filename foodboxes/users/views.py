8b8bda9be9f1316a2a4e5063cc1d08d75cfaf807
Arif_Gafizov <agafizov@gmail.com>
2021-02-11 16:32:07 +0300

    Update authorization

--- foodboxes/users/views.py
+++ foodboxes/users/views.py
@@ -1,10 +1,8 @@
 from rest_framework import status
 from rest_framework.authentication import TokenAuthentication
-from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.generics import CreateAPIView, RetrieveUpdateAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from rest_framework.authtoken.models import Token
 
 from .models import User
 from .serializers import UserSerializer
@@ -31,15 +29,3 @@ class RegisterUserView(CreateAPIView):
         data = serializer.data
 
         return Response(data, status=status.HTTP_201_CREATED)
-
-
-class CustomAuthToken(ObtainAuthToken):
-    def post(self, request, *args, **kwargs):
-        serializer = self.serializer_class(data=request.data,
-                                           context={'request': request})
-        serializer.is_valid(raise_exception=True)
-        user = serializer.validated_data['user']
-        token, created = Token.objects.get_or_create(user=user)
-        return Response({
-            'token': token.key,
-        })
