8c72fe9fc956466a1e7f6bc8a88317e38b76f5a8
Arif_Gafizov <agafizov@gmail.com>
2021-02-11 17:00:01 +0300

    Handle a unique login

--- foodboxes/users/views.py
+++ foodboxes/users/views.py
@@ -1,6 +1,9 @@
+from django.db import IntegrityError
+from rest_framework import status
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.generics import CreateAPIView, RetrieveUpdateAPIView
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from .models import User
 from .serializers import UserSerializer
@@ -19,3 +22,11 @@ class CurrentUserRetrieveUpdateView(RetrieveUpdateAPIView):
 class RegisterUserView(CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    # handle a unique login
+    def create(self, request, *args, **kwargs):
+        try:
+            return super(CreateAPIView, self).create(request, *args, **kwargs)
+        except IntegrityError:
+            content = {'error': 'IntegrityError, please enter other username'}
+            return Response(content, status=status.HTTP_400_BAD_REQUEST)
