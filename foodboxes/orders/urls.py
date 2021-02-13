a3ec3c1bf1125b368bfbcefede74e60c28e947e8
Arif_Gafizov <agafizov@gmail.com>
2021-02-13 19:43:51 +0300

    Add path orders_detail

--- foodboxes/orders/urls.py
+++ foodboxes/orders/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from .views import OrderListCreate
+from .views import OrderListCreate, OrderItemDetail
 
 
 urlpatterns = [
     path('', OrderListCreate.as_view(), name='orders_list'),
-    #path('<int:pk>/', ItemDetail.as_view(), name='items_detail'),
+    path('<int:pk>/', OrderItemDetail.as_view(), name='orders_detail'),
 ]
