628cef14bb07dd0aa0748d6655a9b38784e04ae6
Arif_Gafizov <agafizov@gmail.com>
2021-02-13 19:44:51 +0300

    Add OrderItemDetail

--- foodboxes/orders/views.py
+++ foodboxes/orders/views.py
@@ -1,14 +1,24 @@
 from rest_framework.authentication import TokenAuthentication
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView
 from rest_framework.permissions import IsAuthenticated
 
 from .models import Order
-from .serializers import OrderSerializer
+from .serializers import OrderListSerializer, OrderDetailSerializer
 
 
 class OrderListCreate(ListCreateAPIView):
     queryset = Order.objects.all()
-    serializer_class = OrderSerializer
+    serializer_class = OrderListSerializer
+    permission_classes = [IsAuthenticated]
+    authentication_classes = (TokenAuthentication,)
+
+    def get_queryset(self):
+        return Order.objects.filter(recipient=self.request.user)
+
+
+class OrderItemDetail(RetrieveUpdateAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderDetailSerializer
     permission_classes = [IsAuthenticated]
     authentication_classes = (TokenAuthentication,)
 
