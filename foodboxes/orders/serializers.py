34adf7528a6b87e2764bfd89bfc8a55ace194ae2
Arif_Gafizov <agafizov@gmail.com>
2021-02-13 19:42:59 +0300

    Add OrderDetailSerializer and rename OrderListSerializer

--- foodboxes/orders/serializers.py
+++ foodboxes/orders/serializers.py
@@ -5,9 +5,18 @@ from carts.serializers import CartSerializer
 from users.serializers import UserSerializer
 
 
-class OrderSerializer(ModelSerializer):
+class OrderListSerializer(ModelSerializer):
     cart = CartSerializer()
 
     class Meta:
         model = Order
         fields = ['id', 'cart', 'status', 'total_cost', 'address', 'delivery_at', 'created_at']
+
+
+class OrderDetailSerializer(ModelSerializer):
+    cart = CartSerializer()
+    recipient = UserSerializer
+
+    class Meta:
+        model = Order
+        fields = ['id', 'cart', 'status', 'recipient', 'total_cost', 'address', 'delivery_at', 'created_at']
