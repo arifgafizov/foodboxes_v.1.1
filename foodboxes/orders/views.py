9acc7dc9031d834136cc4e2c72c9b07a3087be5a
Arif_Gafizov <agafizov@gmail.com>
2021-02-14 09:47:53 +0300

    Override update method of OrderItemDetail

--- foodboxes/orders/views.py
+++ foodboxes/orders/views.py
@@ -1,9 +1,11 @@
+from rest_framework import status
 from rest_framework.authentication import TokenAuthentication
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView, get_object_or_404
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from .models import Order
-from .serializers import OrderListSerializer, OrderDetailSerializer
+from .serializers import OrderListSerializer, OrderDetailSerializer, OrderUpdateSerializer
 
 
 class OrderListCreate(ListCreateAPIView):
@@ -22,5 +24,17 @@ class OrderItemDetail(RetrieveUpdateAPIView):
     permission_classes = [IsAuthenticated]
     authentication_classes = (TokenAuthentication,)
 
+
     def get_queryset(self):
         return Order.objects.filter(recipient=self.request.user)
+
+    def update(self, request, *args, **kwargs):
+        order = get_object_or_404(self.queryset.filter(recipient=self.request.user))
+        serializer = OrderUpdateSerializer(order, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+
+        if serializer.validated_data.get('status') == 'created':
+            serializer.save()
+            data = serializer.data
+            return Response(data, status=status.HTTP_200_OK)
+        return Response('Order status is not created', status=status.HTTP_400_BAD_REQUEST)
